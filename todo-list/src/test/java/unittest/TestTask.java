7802ca13c5f002d90a98c85aacc1ba9b8ee57b80
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-02-19 20:05:47 -0300

    include more unit tests

--- todo-list/src/test/java/unittest/TestTask.java
+++ todo-list/src/test/java/unittest/TestTask.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.redhat.model.Task;
