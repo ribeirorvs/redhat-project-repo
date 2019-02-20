a43df45fed3428b8c9df884960ed1bab524dfb6c
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-02-19 21:23:10 -0300

    Include unit tests to ToDo

--- todo-list/src/test/java/unittest/TestToDo.java
+++ todo-list/src/test/java/unittest/TestToDo.java
@@ -2,48 +2,83 @@ package unittest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import com.redhat.model.Task;
+import com.redhat.model.ToDo;
 
 class TestToDo {
 
 	@Test
 	void testToDo() {
-		fail("Not yet implemented"); // TODO
+		ToDo td = new ToDo();
+		assertNotNull(td, "ToDo can't be created");
 	}
 
 	@Test
 	void testToDoListOfTask() {
-		fail("Not yet implemented"); // TODO
+		List<Task> tasks = new ArrayList<Task>();
+		for(int i = 0; i < 3; i++) {
+			Task t = new Task();
+			tasks.add(t);
+		}
+		ToDo td = new ToDo(tasks);
+		assertNotNull(td);
 	}
 
 	@Test
 	void testGetTasks() {
-		fail("Not yet implemented"); // TODO
+		List<Task> tasks = new ArrayList<Task>();
+		for(int i = 0; i < 3; i++) {
+			Task t = new Task();
+			tasks.add(t);
+		}
+		ToDo td = new ToDo(tasks);
+		assertEquals(td.getTasks(), tasks, "Error to get Tasks");
 	}
 
 	@Test
 	void testSetTasks() {
-		fail("Not yet implemented"); // TODO
+		List<Task> tasks = new ArrayList<Task>();
+		for(int i = 0; i < 3; i++) {
+			Task t = new Task();
+			tasks.add(t);
+		}
+		ToDo td = new ToDo();
+		td.setTasks(tasks);
+		assertEquals(td.getTasks(), tasks, "Error to set Tasks");
 	}
 
 	@Test
 	void testAddTask() {
-		fail("Not yet implemented"); // TODO
+		Task t = new Task();
+		ToDo td = new ToDo();
+		td.addTask(t);
+		assertNotNull(td.getTasks(), "Error to add a Task");
+		assertEquals(td.getTasks().size(), 1, "Error to add a Task");
 	}
 
 	@Test
 	void testRemoveTask() {
-		fail("Not yet implemented"); // TODO
+		Task t = new Task();
+		ToDo td = new ToDo();
+		td.addTask(t);
+		assertNotNull(td.getTasks(), "Error to add a Task");
+		assertEquals(td.getTasks().size(), 1, "Error to add a Task");
+		
+		td.removeTask(t);
+		assertEquals(td.getTasks().size(), 0, "Error to remove a Task");
 	}
 
 	@Test
 	void testClearToDo() {
-		fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	void testToString() {
-		fail("Not yet implemented"); // TODO
 	}
 
 }
