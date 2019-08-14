0c23a8313d97914ac5268086e1d56116ee5173af
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-08-14 20:45:05 -0300

    Complete the code style

--- todo-list/src/main/java/com/redhat/model/Task.java
+++ todo-list/src/main/java/com/redhat/model/Task.java
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 package com.redhat.model;
 
 import java.io.Serializable;
@@ -9,100 +5,205 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 /**
- * @version		1.0
- * @author		Rodrigo Vitor Ribeiro
+ * @version 1.0
+ * @author Rodrigo Vitor Ribeiro
  */
-
 public class Task implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private static int total = 0;
-	
-	private int id;
-	private LocalDate startDay, finalDay;
-	private LocalTime startTime, finalTime;
-	private String title;
-	private String description;
-	
-	
-	public Task() {
-		this.id = total;
-		total++;
-	}
-	
-	public Task(LocalDate startDay, LocalDate finalDay, LocalTime startTime, 
-			LocalTime finalTime, String title, String description) {
-		this();
-		this.startDay = startDay;
-		this.finalDay = finalDay;
-		this.startTime = startTime;
-		this.finalTime = finalTime;
-		this.title = title;
-		this.description = description;
-	}
 
-	public LocalDate getStartDay() {
-		return startDay;
-	}
+    private static final long serialVersionUID = 1L;
+    private static int total = 0;
+    private int id;
+    private LocalDate startDay, finalDay;
+    private LocalTime startTime, finalTime;
+    private String title;
+    private String description;
 
-	public void setStartDay(LocalDate startDay) {
-		this.startDay = startDay;
-	}
+    /**
+     * Default constructor.
+     */
+    public Task() {
+        this.id = total;
+        total++;
+    }
 
-	public LocalDate getFinalDay() {
-		return finalDay;
-	}
+    /**
+     * Constructor that receive all attributes of the class.
+     *
+     * @param startDayVal The start day of the task,
+     * need to be => to actual date
+     * @param finalDayVal The final day of the task need to be => to startDay
+     * @param startTimeVal The start time of the task,
+     * need to be > to actual time
+     * @param finalTimeVal If the finalDay is the same as startDay,
+     * this need to be > then startTime
+     * @param titleVal The title of the task
+     * @param descriptionVal The description of the task, it can be null
+     */
+    public Task(final LocalDate startDayVal,
+                final LocalDate finalDayVal,
+                final LocalTime startTimeVal,
+                final LocalTime finalTimeVal,
+                final String titleVal,
+                final String descriptionVal) {
+        this();
+        this.startDay = startDayVal;
+        this.finalDay = finalDayVal;
+        this.startTime = startTimeVal;
+        this.finalTime = finalTimeVal;
+        this.title = titleVal;
+        this.description = descriptionVal;
+    }
 
-	public void setFinalDay(LocalDate finalDay) {
-		this.finalDay = finalDay;
-	}
+    /**
+     * This method return only the start day of the task.
+     *
+     * @return Return a LocalDate refer to start day of the task
+     * @see LocalDate
+     */
+    public LocalDate getStartDay() {
+        return startDay;
+    }
 
-	public LocalTime getStartTime() {
-		return startTime;
-	}
+    /**
+     * This method change the start day of the task.
+     *
+     * @param newStartDay A LocalDate object that,
+     * will be the new start day of the task
+     * @see LocalDate
+     */
+    public void setStartDay(final LocalDate newStartDay) {
+        this.startDay = newStartDay;
+    }
 
-	public void setStartTime(LocalTime startTime) {
-		this.startTime = startTime;
-	}
+    /**
+     * This method return only the final day of the task.
+     *
+     * @return Return a LocalDate refer to final day of the task
+     * @see LocalDate
+     */
+    public LocalDate getFinalDay() {
+        return finalDay;
+    }
 
-	public LocalTime getFinalTime() {
-		return finalTime;
-	}
+    /**
+     * This method set a new final date to the task.
+     *
+     * @param newFinalDay A LocalDate that will be the
+     * new final date of the task
+     * @see LocalDate
+     */
+    public void setFinalDay(final LocalDate newFinalDay) {
+        this.finalDay = newFinalDay;
+    }
 
-	public void setFinalTime(LocalTime finalTime) {
-		this.finalTime = finalTime;
-	}
+    /**
+     * This method return the start time of the task.
+     *
+     * @return Return a LocalTime that represent the start time of the task.
+     * @see LocalTime
+     */
+    public LocalTime getStartTime() {
+        return startTime;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    /**
+     * This method set a new start time to the task.
+     *
+     * @param newStartTime A LocalTime that will be the
+     * new start time of the task.
+     * @see LocalTime
+     */
+    public void setStartTime(final LocalTime newStartTime) {
+        this.startTime = newStartTime;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    /**
+     * This method return the final time of the task.
+     *
+     * @return Return a LocalTime that represent the final time of the task.
+     * @see LocalTime
+     */
+    public LocalTime getFinalTime() {
+        return finalTime;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    /**
+     * This method set a new final time to the task.
+     *
+     * @param newFinalTime A LocalTime that will be the
+     * new final time of the task.
+     * @see LocalTime
+     */
+    public void setFinalTime(final LocalTime newFinalTime) {
+        this.finalTime = newFinalTime;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	@Override
-	public String toString() {
-		return "Task [Id=" + id + ", titile=" + this.title + ", From=" + this.startDay + 
-				" " + this.startTime + ", to=" + this.finalDay + " " + 
-				this.finalTime + ", description=" + this.description + "]";
-	}
-	
+    /**
+     * This method return the task title.
+     *
+     * @return Return a task title in a String
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * This method set a new task title.
+     *
+     * @param newTitle A String that will be the new task title.
+     */
+    public void setTitle(final String newTitle) {
+        this.title = newTitle;
+    }
+
+    /**
+     * This method return the task description.
+     *
+     * @return Return the task description like a String
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * This method set a new task description.
+     *
+     * @param newDescription A String that will be the new task description
+     */
+    public void setDescription(final String newDescription) {
+        this.description = newDescription;
+    }
+
+    /**
+     * This method return the ID of the task.
+     *
+     * @return Return a task id like a int
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * This method set a new task ID.
+     * <p>
+     * Use this method with careful, it don't have a validation.
+     *
+     * @param newId A int that will be the new task ID
+     */
+    public void setId(final int newId) {
+        this.id = newId;
+    }
+
+    /**
+     * This method override the default toString method
+     * to return a more useful string about the class.
+     */
+    @Override
+    public String toString() {
+        return "Task [Id=" + id
+                + ", titile=" + this.title
+                + ", From=" + this.startDay + " " + this.startTime
+                + ", to=" + this.finalDay + " " + this.finalTime
+                + ", description=" + this.description + "]";
+    }
 }
