fe279850a4f01f30f6ab0b4e07b27d9af1f3dc34
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-08-14 21:19:10 -0300

    Some changes to the code style

--- todo-list/src/main/java/com/redhat/controller/ToDoController.java
+++ todo-list/src/main/java/com/redhat/controller/ToDoController.java
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 package com.redhat.controller;
 
 import java.io.BufferedReader;
@@ -11,99 +7,100 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.redhat.model.Task;
 import com.redhat.model.ToDo;
 
 /**
- * @version 	2.0
- * @author 		Rodrigo Vitor Ribeiro
+ * @version 2.0
+ * @author Rodrigo Vitor Ribeiro
  */
-
 public class ToDoController {
-	
-	private List<TaskController> tasks = new ArrayList<TaskController>();
-	
-	public ToDoController() {}
-	
-	public ToDoController(ToDo todo) {
-		for (Task task : todo.getTasks()) {
-			this.tasks.add(new TaskController(task));
-		}
-	}
-	
-	public List<TaskController> getTasks() {
-		return tasks;
-	}
 
-	public void setTasks(List<TaskController> tasks) {
-		this.tasks = tasks;
-	}
-	
-	public void addTask(TaskController task) {
-		this.tasks.add(task);
-	}
-	
-	/**
-	 * The method addTask are created to support non GUI application.
-	 * Method related with console application version  	
-	 */
-	public void addTask() throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		TaskController task = new TaskController();
-		System.out.print("Title of the task: ");
-		task.setTitle(reader.readLine());
-		System.out.print("Start day (yyyy-MM-dd): ");
-		task.setStartDay(LocalDate.parse(reader.readLine()));
-		System.out.print("Start time (HH:mm:ss): ");
-		task.setStartTime(LocalTime.parse(reader.readLine()));
-		System.out.print("End day (yyyy-MM-dd): ");
-		task.setFinalDay(LocalDate.parse(reader.readLine()));
-		System.out.print("End time (HH:mm:ss): ");
-		task.setFinalTime(LocalTime.parse(reader.readLine()));
-		System.out.print("Description : ");
-		task.setDescription(reader.readLine());
-		addTask(task);
-	}
-	
-	public TaskController checkTask(int id){
-		for (TaskController task : tasks) {
-			if(task.getId() == id) {
-				return task;
-			}
-		}
-		return null;
-	}
-	
-	public void removeTask(TaskController task) {
-		this.tasks.remove(task);
-	}
-	
-	public void removeTask(int id){
-		TaskController task = this.checkTask(id);
-		this.removeTask(task);
-	}
-	
-	public void clearToDo() {
-		this.tasks.clear();
-	}
-	
-	@Override
-	public String toString() {
-		String todo = "ToDoController {\n";
-		for (TaskController task : tasks) {
-			todo = todo.concat(task.toString() + "\n");
-		}
-		todo = todo.concat("}");
-		return todo;
-	}
-	
-	public ToDo toTodo() {
-		List<Task> task = new ArrayList<Task>();
-		for (TaskController taskController : this.tasks) {
-			task.add(taskController.toTask());
-		}
-		ToDo todo = new ToDo(task);
-		return todo;
-	}
+    private List<TaskController> tasks = new ArrayList<TaskController>();
+
+    /**
+     * Default constructor.
+     */
+    public ToDoController() {}
+
+    public ToDoController(ToDo todo) {
+        for (Task task : todo.getTasks()) {
+            this.tasks.add(new TaskController(task));
+        }
+    }
+
+    public List<TaskController> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskController> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(TaskController task) {
+        this.tasks.add(task);
+    }
+
+    /**
+     * The method addTask are created to support non GUI application.
+     * Method related with console application version  	
+     */
+    public void addTask() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        TaskController task = new TaskController();
+        System.out.print("Title of the task: ");
+        task.setTitle(reader.readLine());
+        System.out.print("Start day (yyyy-MM-dd): ");
+        task.setStartDay(LocalDate.parse(reader.readLine()));
+        System.out.print("Start time (HH:mm:ss): ");
+        task.setStartTime(LocalTime.parse(reader.readLine()));
+        System.out.print("End day (yyyy-MM-dd): ");
+        task.setFinalDay(LocalDate.parse(reader.readLine()));
+        System.out.print("End time (HH:mm:ss): ");
+        task.setFinalTime(LocalTime.parse(reader.readLine()));
+        System.out.print("Description : ");
+        task.setDescription(reader.readLine());
+        addTask(task);
+    }
+
+    public TaskController checkTask(int id){
+        for (TaskController task : tasks) {
+            if(task.getId() == id) {
+                return task;
+            }
+        }
+        return null;
+    }
+
+    public void removeTask(TaskController task) {
+        this.tasks.remove(task);
+    }
+
+    public void removeTask(int id){
+        TaskController task = this.checkTask(id);
+        this.removeTask(task);
+    }
+
+    public void clearToDo() {
+        this.tasks.clear();
+    }
+
+    @Override
+    public String toString() {
+        String todo = "ToDoController {\n";
+        for (TaskController task : tasks) {
+            todo = todo.concat(task.toString() + "\n");
+        }
+        todo = todo.concat("}");
+        return todo;
+    }
+
+    public ToDo toTodo() {
+        List<Task> task = new ArrayList<Task>();
+        for (TaskController taskController : this.tasks) {
+            task.add(taskController.toTask());
+        }
+        ToDo todo = new ToDo(task);
+        return todo;
+    }
 }
---
+++ todo-list/src/main/java/com/redhat/controller/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Provides the controllers of the application.
+ * <p>
+ * All business rules of the application are in this package
+ *
+ * @version 2.5
+ * @author Rodrigo Vitor Ribeiro
+ */
+package com.redhat.controller;
