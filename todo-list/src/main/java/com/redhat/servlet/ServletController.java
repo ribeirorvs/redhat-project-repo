3d45f81d3f4d933c2ba3ead79b3050a257ceb606
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-08-14 21:07:33 -0300

    Complete the code style

--- todo-list/src/main/java/com/redhat/servlet/ServletController.java
+++ todo-list/src/main/java/com/redhat/servlet/ServletController.java
@@ -1,75 +1,124 @@
-/*
- * 
- */
 package com.redhat.servlet;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.redhat.controller.TaskController;
 import com.redhat.controller.ToDoController;
 
 /**
- * @version		2.5 13 Aug 2019
- * @author 		Rodrigo Vitor Ribeiro
+ * @version 2.5 13 Aug 2019
+ * @author Rodrigo Vitor Ribeiro
  */
-public class ServletController extends HttpServlet{
-	private static final long serialVersionUID = 1L;
-	private ToDoController td = new ToDoController();
-	
-	public void doPost(HttpServletRequest request, 
-						HttpServletResponse response) throws ServletException, IOException {
-		switch(request.getParameter("operation")) {
-		case "addTask":
-			addTask(request, response, td);
-			break;
-		case "listTasks":
-			listTasks(request, response, td);
-			break;
-		case "checkTask":
-			checkTask(request, response, td);
-			break;
-		}
-		
-		RequestDispatcher view = request.getRequestDispatcher("result.jsp");
-		view.forward(request, response);
-		
-	}
-	
-	private void addTask(HttpServletRequest request, 
-						HttpServletResponse response,
-						ToDoController td) {
-		TaskController t = new TaskController();
-		t.setDescription(request.getParameter("task-desc"));
-		t.setTitle(request.getParameter("task-title"));
-		t.setStartDay(LocalDate.parse(request.getParameter("task-startDay")));
-		t.setStartTime(LocalTime.parse(request.getParameter("task-startTime")));
-		t.setFinalDay(LocalDate.parse(request.getParameter("task-lastDay")));
-		t.setFinalTime(LocalTime.parse(request.getParameter("task-finalTime")));
-		
-		td.addTask(t);
-		
-		request.setAttribute("styles", "Task added");
-	}
-	
-	private void listTasks(HttpServletRequest request, 
-			HttpServletResponse response,
-			ToDoController td) {
-		request.setAttribute("styles", td.toString());
-	}
-	
-	private void checkTask(HttpServletRequest request,
-							HttpServletResponse response,
-							ToDoController td) {
-		request.setAttribute("style", td.checkTask(
-						Integer.parseInt(request.getParameter("id"))));
-	}
-	
+public class ServletController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private ToDoController td = new ToDoController();
+
+    /**
+     * The doPost method that controlle what the servlet will do.
+     * @param request A request with the parameters what
+     * will used to add the task.
+     * @param response A response that can be used to return the method result,
+     * but is not used in this case.
+     * @throws ServletException Return error from servlet
+     * @throws IOException Return error from IO
+     * @see HttpServletRequest
+     * @see HttpServletResponse
+     */
+    public void doPost(final HttpServletRequest request,
+                        final HttpServletResponse response)
+                                throws ServletException, IOException {
+        switch (request.getParameter("operation")) {
+        case "addTask":
+            addTask(request, response, td);
+            break;
+        case "listTasks":
+            listTasks(request, response, td);
+            break;
+        case "checkTask":
+            checkTask(request, response, td);
+            break;
+        default:
+            break;
+        }
+
+        RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
+
+    }
+
+    /**
+     * This method use the ToDoController to add a new task to TO DO list.
+     *
+     * @param request A request with the parameters what
+     * will used to add the task.
+     * @param response A response that can be used to return the method result,
+     * but is not used in this case.
+     * @param tmpTd The ToDoController that will be
+     * responsible for add the task.
+     * @see HttpServletRequest
+     * @see HttpServletResponse
+     * @see ToDoController
+     */
+    private void addTask(final HttpServletRequest request,
+            final HttpServletResponse response,
+            final ToDoController tmpTd) {
+        TaskController t = new TaskController();
+        t.setDescription(request.getParameter("task-desc"));
+        t.setTitle(request.getParameter("task-title"));
+        t.setStartDay(LocalDate.parse(request.getParameter("task-startDay")));
+        t.setStartTime(LocalTime.parse(request.getParameter("task-startTime")));
+        t.setFinalDay(LocalDate.parse(request.getParameter("task-lastDay")));
+        t.setFinalTime(LocalTime.parse(request.getParameter("task-finalTime")));
+
+        tmpTd.addTask(t);
+
+        request.setAttribute("styles", "Task added");
+    }
+
+    /**
+     * This method take the list of tasks in the TO DO list.
+     *
+     * @param request A request that can be used to perform actions,
+     * but is not used in this method.
+     * @param response A response that can be used to return the method result,
+     * but is not used in this case.
+     * @param tmpTd The ToDoController that will be
+     * responsible for list the tasks
+     * @see HttpServletRequest
+     * @see HttpServletResponse
+     * @see ToDoController
+     */
+    private void listTasks(final HttpServletRequest request,
+            final HttpServletResponse response,
+            final ToDoController tmpTd) {
+        request.setAttribute("styles", tmpTd.toString());
+    }
+
+    /**
+     * This method take a Task from the TO DO list.
+     *
+     * @param request A request with the parameters what
+     * will used to add the task.
+     * @param response A response that can be used to return the method result,
+     * but is not used in this case.
+     * @param tmpTd The ToDoController that will be
+     * responsible for add the task.
+     * @see HttpServletRequest
+     * @see HttpServletResponse
+     * @see ToDoController
+     */
+    private void checkTask(final HttpServletRequest request,
+            final HttpServletResponse response,
+            final ToDoController tmpTd) {
+        request.setAttribute("style", tmpTd.checkTask(
+                Integer.parseInt(request.getParameter("id"))));
+    }
+
 }
---
+++ todo-list/src/main/java/com/redhat/servlet/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the Servlets of the project.
+ *
+ * @version 2.5
+ * @author Rodrigo Vitor Ribeiro
+ */
+package com.redhat.servlet;
