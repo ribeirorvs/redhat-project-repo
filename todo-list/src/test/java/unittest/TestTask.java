0b3ecbee5e7783d2970d8722806410c726ae51c0
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2019-08-19 22:30:34 -0300

    Include comments

--- todo-list/src/test/java/unittest/TestTask.java
+++ todo-list/src/test/java/unittest/TestTask.java
@@ -9,82 +9,103 @@ import org.junit.jupiter.api.Test;
 
 import com.redhat.model.Task;
 
+/**
+ * Test class created to granter the
+ * functionality of the Task class.
+ *
+ * @author Rodrigo Vitor Ribeiro
+ */
 class TestTask {
 
-	@Test
-	void testTask() {
-		Task t = new Task();
-		assertNotNull(t, "Task can't be instanteated!");
-	}
+    /**
+     * Test if a Task can be instantiated
+     *
+     * @author Rodrigo Vitor Ribeiro
+     */
+    @Test
+    void testCreateTask() {
+        Task t = new Task();
+        assertNotNull(t, "Task can't be instanteated!");
+    }
 
-	@Test
-	void testTaskLocalDateLocalDateLocalTimeLocalTimeStringString() {
-		LocalDate startDay, endDay;
-		LocalTime startTime, endTime;
-		
-		startDay = LocalDate.parse("2018-12-30");
-		endDay = LocalDate.now();
-		
-		startTime = LocalTime.parse("00:00");
-		endTime = LocalTime.now();
-		
-		
-		Task t = new Task(startDay, endDay, startTime, endTime,
-							"Task title", "Task description");
-		assertNotNull(t, "Task can't be instanteated!");
-	}
+    /**
+     * Test if a Task can be instantiated with all parameters
+     *
+     * @author Rodrigo Vitor Ribeiro
+     */
+    @Test
+    void testTaskLocalDateLocalDateLocalTimeLocalTimeStringString() {
+        LocalDate startDay, endDay;
+        LocalTime startTime, endTime;
 
-	@Test
-	void testSetStartDay() {
-		Task t = new Task();
-		LocalDate day = LocalDate.now();
-		t.setStartDay(day);
-		assertEquals(t.getStartDay(), day, "Incorret start day");
-	}
+        startDay = LocalDate.parse("2018-12-30");
+        endDay = LocalDate.now();
 
-	@Test
-	void testSetFinalDay() {
-		Task t = new Task();
-		LocalDate day = LocalDate.now();
-		t.setFinalDay(day);
-		assertEquals(t.getFinalDay(), day, "Incorret final day");
-	}
+        startTime = LocalTime.parse("00:00");
+        endTime = LocalTime.now();
 
-	@Test
-	void testSetStartTime() {
-		Task t = new Task();
-		LocalTime time = LocalTime.now();
-		t.setStartTime(time);
-		assertEquals(t.getStartTime(), time, "Incorret start time");
-	}
 
-	@Test
-	void testSetFinalTime() {
-		Task t = new Task();
-		LocalTime time = LocalTime.now();
-		t.setFinalTime(time);
-		assertEquals(t.getFinalTime(), time, "Incorret final time");
-	}
+        Task t = new Task(startDay, endDay, startTime, endTime,
+                "Task title", "Task description");
+        assertNotNull(t, "Task can't be instanteated!");
+    }
 
-	@Test
-	void testSetTitle() {
-		Task t = new Task();
-		t.setTitle("Task title");
-		assertEquals(t.getTitle(), "Task title", "Incorrect title");
-	}
+    /**
+     * Test the method setStartDay
+     *
+     * @author Rodrigo Vitor Ribeiro
+     */
+    @Test
+    void testSetStartDay() {
+        Task t = new Task();
+        LocalDate day = LocalDate.now();
+        t.setStartDay(day);
+        assertEquals(t.getStartDay(), day, "Incorret start day");
+    }
 
-	@Test
-	void testSetDescription() {
-		Task t = new Task();
-		t.setDescription("Task description");
-		assertEquals(t.getDescription(), "Task description", "Incorrect decription");
-	}
+    @Test
+    void testSetFinalDay() {
+        Task t = new Task();
+        LocalDate day = LocalDate.now();
+        t.setFinalDay(day);
+        assertEquals(t.getFinalDay(), day, "Incorret final day");
+    }
 
-	@Test
-	void testSetId() {
-		Task t = new Task();
-		t.setId(1000);
-		assertEquals(t.getId(), 1000, "");
-	}
+    @Test
+    void testSetStartTime() {
+        Task t = new Task();
+        LocalTime time = LocalTime.now();
+        t.setStartTime(time);
+        assertEquals(t.getStartTime(), time, "Incorret start time");
+    }
+
+    @Test
+    void testSetFinalTime() {
+        Task t = new Task();
+        LocalTime time = LocalTime.now();
+        t.setFinalTime(time);
+        assertEquals(t.getFinalTime(), time, "Incorret final time");
+    }
+
+    @Test
+    void testSetTitle() {
+        Task t = new Task();
+        t.setTitle("Task title");
+        assertEquals(t.getTitle(), "Task title", "Incorrect title");
+    }
+
+    @Test
+    void testSetDescription() {
+        Task t = new Task();
+        t.setDescription("Task description");
+        assertEquals(t.getDescription(), "Task description", "Incorrect decription");
+    }
+
+    @Test
+    void testSetId() {
+        Task t = new Task();
+        t.setId(1000);
+        assertEquals(t.getId(), 1000, "");
+    }
 
 }
